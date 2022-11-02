b34984f2a568b01ba30ca9a808e80b998b09e253
Inaki Huete Nieves <inaki.huetenieves@maibornwolff.de>
2022-11-02 17:17:11 +0100

    Initial Commit

---
+++ src/main/java/com/example/hello/jakarta/HelloJakartaRestApplication.java
@@ -0,0 +1,11 @@
+package com.example.hello.jakarta;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class HelloJakartaRestApplication extends Application {
+}
