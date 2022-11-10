6adb35d51cca79fd61302001d99296f994e8f6a5
Inaki Huete Nieves <inaki.huetenieves@maibornwolff.de>
2022-11-10 08:06:44 +0100

    Add more logging configuration example

--- src/main/java/com/example/hello/jakarta/hello/HelloController.java
+++ src/main/java/com/example/hello/jakarta/hello/HelloController.java
@@ -4,6 +4,8 @@ import com.example.hello.jakarta.util.Logged;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -12,9 +14,13 @@ import jakarta.ws.rs.Path;
 @Singleton
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GET
     @Logged
     public String sayHello() {
+        log.debug("hello debug");
+        log.info("hello info");
 
         return "Hello World";
     }
